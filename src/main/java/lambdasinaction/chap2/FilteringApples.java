21c2dbcad50fe9310c8fcb63272fa95498e7a4d4
edidada <sandisks555@gmail.com>
2019-10-10 16:55:54 +0800

    add some

--- src/main/java/lambdasinaction/chap2/FilteringApples.java
+++ src/main/java/lambdasinaction/chap2/FilteringApples.java
@@ -1,12 +1,15 @@
 package lambdasinaction.chap2;
 
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class FilteringApples{
 
 	public static void main(String ... args){
 
-		List<Apple> inventory = Arrays.asList(new Apple(80,"green"), new Apple(155, "green"), new Apple(120, "red"));	
+		List<Apple> inventory = Arrays.asList(new Apple(80,"green"), new Apple(155, "green"), new Apple(120, "red"));
 
 		// [Apple{color='green', weight=80}, Apple{color='green', weight=155}]
 		List<Apple> greenApples = filterApplesByColor(inventory, "green");
